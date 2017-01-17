eeeeb54f2119cdf91b538717d645153641c4e954
Subins Jose <subinsjose@gmail.com>
2017-01-17 16:40:07 +0530

    Created cake details html page

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^cake/(?P<pk>\d+)/$', views.cake_details, name='cake_details'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Cake 
 
 def post_list(request):
     cakes = Cake.objects.all()
     return render(request, 'blog/post_list.html', {'cakes' : cakes})
+
+def cake_details(request, pk):
+    cake = get_object_or_404(Cake, pk=pk)
+    return render(request, 'blog/cake_details.html', {'cake' : cake})
